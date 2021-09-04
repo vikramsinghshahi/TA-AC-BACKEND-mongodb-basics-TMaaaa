1244f0cc66acc88bd1bc51fa1d2e674f55ee3eeb
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-09-04 21:51:45 +0530

    block-BMaaah

---
+++ block-BMaaah/code.js
@@ -0,0 +1,35 @@
+var articles = [
+  {
+    title: 'Tracking',
+    Date: 12 / 14 / 2021,
+    details: 'all about tracking',
+    author: {
+      name: 'vikram',
+      email: '123@gamil.com',
+      age: 24,
+    },
+    tag: ['html', 'css'],
+  },
+  {
+    title: 'paragliding',
+    Date: 12 - 14 - 2021,
+    details: 'all about paragliding',
+    author: {
+      name: 'rahul',
+      email: '1234@gamil.com',
+      age: 25,
+    },
+    tag: ['html', 'css'],
+  },
+  {
+    title: 'Tracking',
+    Date: 12 - 14 - 2021,
+    details: 'all about tracking',
+    author: {
+      name: 'vikram',
+      email: '12345@gamil.com',
+      age: 26,
+    },
+    tag: ['html', 'css'],
+  },
+];
